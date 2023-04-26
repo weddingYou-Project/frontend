b9271b88c1831af03fdeea9e85666d632f103070
diana815 <diana815@naver.com>
2023-04-26 17:16:41 +0900

    feat : login, signup, passwordsearch, passwordchange, update layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 function App() {
-  return <div>lala</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/signup" element={<Signup />}></Route>
+      <Route path="/signup/user" element={<SignUpUser />}></Route>
+      <Route path="/signup/planner" element={<SignUpPlanner />}></Route>
+      <Route path="/passwordSearch" element={<PasswordSearch />} />
+      <Route
+        path="/passwordSearch/temporaryPasswordLogin"
+        element={<TemporaryPasswordLogin />}
+      />
+      <Route
+        path="/passwordSearch/temporaryPasswordLogin/passwordChange"
+        element={<PasswordChange />}
+      />
+      <Route path="/mypage/:category" element={<MyPage />} />
+      <Route path="/mypage/:category/userupdate" element={<UserUpdate />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
 export default App;
