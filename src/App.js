1bf812ce0a74c166363ecf86e5c41703a9ad914f
diana815 <diana815@naver.com>
2023-04-27 10:00:31 +0900

    feat : index 에 browserrouter 추가, pages 파일명 수정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
-import SignUpForm from "./Pages/SignUpForm";
+import SignupForm from "./Pages/SignupForm";
 import PasswordSearch from "./Pages/PasswordSearch";
 import TemporaryPasswordLogin from "./Pages/TemporaryPasswordLogin";
 import PasswordChange from "./Pages/PasswordChange";
-import MyPage from "./Pages/MyPage";
+import Mypage from "./Pages/Mypage";
 import UserUpdate from "./Pages/UserUpdate";
 import NotFound from "./Pages/NotFound";
 
@@ -16,7 +16,7 @@ function App() {
       <Route path="/" element={<Home />}></Route>
       <Route path="/login" element={<Login />}></Route>
       <Route path="/signup" element={<Signup />}></Route>
-      <Route path="/signup/:category" element={<SignUpForm />}></Route>
+      <Route path="/signup/:category" element={<SignupForm />}></Route>
       <Route path="/passwordSearch" element={<PasswordSearch />} />
       <Route
         path="/passwordSearch/temporaryPasswordLogin"
@@ -26,7 +26,7 @@ function App() {
         path="/passwordSearch/temporaryPasswordLogin/passwordChange"
         element={<PasswordChange />}
       />
-      <Route path="/mypage/:category" element={<MyPage />} />
+      <Route path="/mypage/:category" element={<Mypage />} />
       <Route path="/mypage/:category/userupdate" element={<UserUpdate />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
