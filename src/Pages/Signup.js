4732b96f74472c714ba2fb355e4391dfa0c99e67
KyeongSe Song <thdrudtp15@naver.com>
2023-04-27 11:52:15 +0900

    feat : 회원가입 레이아웃CSS

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,5 +1,32 @@
+import "../Css/Signup.css";
+import { useNavigate } from "react-router-dom";
+
 function Signup() {
-  return <div></div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="Signup-wrap">
+      <div className="Signup-backicon"></div>
+      <div className="Signup-header"></div>
+      <div className="Signup-logo"></div>
+      <div className="Signup-button">
+        <button
+          onClick={() => {
+            navigate("./user");
+          }}
+        >
+          일반회원
+        </button>
+        <button
+          onClick={() => {
+            navigate("./planner");
+          }}
+        >
+          플래너 회원
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
--- src/Pages/SignupForm.js
+++ src/Pages/SignupForm.js
@@ -1,5 +1,16 @@
+import "../Css/Signup.css";
+
 function SignupForm() {
-  return <div></div>;
+  return (
+    <div className="Signup-wrap">
+      <div className="Signup-backicon"></div>
+      <div className="Signup-header"></div>
+      <div className="Signup-guidebar"></div>
+      <div className="Signup-button">
+        <button>회원가입</button>
+      </div>
+    </div>
+  );
 }
 
 export default SignupForm;
