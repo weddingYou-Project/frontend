7ed269a6afa759dd0750e07cb2ae1da611f1e590
diana815 <diana815@naver.com>
2023-04-27 16:51:41 +0900

    feat : myEstimate 내 견적 버튼 추가, navigationbar 패딩 조정

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -1,11 +1,13 @@
 import BackButton from "./BackButton";
 import "../Css/NavigationBar.css";
+import MyEstimate from "./MyEstimate";
 
 function NavigationBar() {
   return (
     <div class="navigationbar">
       <BackButton />
       <p>제목</p>
+      <MyEstimate />
     </div>
   );
 }
