2098ce580adbf5876d26094aa4e8a48a881f395c
diana815 <diana815@naver.com>
2023-04-28 14:48:03 +0900

    feat : mypage 비밀번호 확인 모달창, 패스워드 유효검사, 버튼 수정

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,9 +6,9 @@ function Footer() {
   const navigate = useNavigate();
   const { category } = useParams();
   return (
-    <div class="footer">
+    <div className="footer">
       <div
-        class="icon"
+        className="icon"
         onClick={() => {
           //전체 메뉴 바 보여주기
         }}
@@ -18,7 +18,7 @@ function Footer() {
           width="40"
           height="40"
           fill="currentColor"
-          class="bi bi-collection"
+          className="bi bi-collection"
           viewBox="0 0 16 16"
         >
           <path d="M2.5 3.5a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-11zm2-2a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1h-7zM0 13a1.5 1.5 0 0 0 1.5 1.5h13A1.5 1.5 0 0 0 16 13V6a1.5 1.5 0 0 0-1.5-1.5h-13A1.5 1.5 0 0 0 0 6v7zm1.5.5A.5.5 0 0 1 1 13V6a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-.5.5h-13z" />
@@ -26,7 +26,7 @@ function Footer() {
         <p>전체 메뉴</p>
       </div>
       <div
-        class="icon"
+        className="icon"
         onClick={() => {
           navigate("/");
         }}
@@ -36,7 +36,7 @@ function Footer() {
           width="40"
           height="40"
           fill="currentColor"
-          class="bi bi-calendar-heart"
+          className="bi bi-calendar-heart"
           viewBox="0 0 16 16"
         >
           <path
@@ -47,7 +47,7 @@ function Footer() {
         <p>플래너</p>
       </div>
       <div
-        class="icon"
+        className="icon"
         onClick={() => {
           navigate("/");
         }}
@@ -57,7 +57,7 @@ function Footer() {
           width="40"
           height="40"
           fill="currentColor"
-          class="bi bi-house-heart"
+          className="bi bi-house-heart"
           viewBox="0 0 16 16"
         >
           <path d="M8 6.982C9.664 5.309 13.825 8.236 8 12 2.175 8.236 6.336 5.309 8 6.982Z" />
@@ -66,7 +66,7 @@ function Footer() {
         <p>홈</p>
       </div>
       <div
-        class="icon"
+        className="icon"
         onClick={() => {
           navigate("/");
         }}
@@ -76,7 +76,7 @@ function Footer() {
           width="40"
           height="40"
           fill="currentColor"
-          class="bi bi-calendar2-check"
+          className="bi bi-calendar2-check"
           viewBox="0 0 16 16"
         >
           <path d="M10.854 8.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L7.5 10.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
@@ -86,7 +86,7 @@ function Footer() {
         <p>견적</p>
       </div>
       <div
-        class="icon"
+        className="icon"
         onClick={() => {
           navigate(`/mypage/${category}`);
         }}
@@ -96,7 +96,7 @@ function Footer() {
           width="40"
           height="40"
           fill="currentColor"
-          class="bi bi-person"
+          className="bi bi-person"
           viewBox="0 0 16 16"
         >
           <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
