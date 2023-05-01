d44ae4963c22f6dc04839f7a8f16faeeff4a2752
diana815 <diana815@naver.com>
2023-05-01 10:37:21 +0900

    fix : 로그인페이지 내견적 컴포넌트 없애기

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -10,7 +10,7 @@ function NavigationBar({ title }) {
     <div className="navigationbar">
       <BackButton />
       <p>{title}</p>
-      {path.indexOf("mypage") && path.indexOf("userupdate") === -1 ? (
+      {path.indexOf("mypage") === 1 && path.indexOf("userupdate") === -1 ? (
         <MyEstimate />
       ) : null}
     </div>
