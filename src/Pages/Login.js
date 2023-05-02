cb2a442229e123c58c7284a7b90ffef98dbe00a2
Jemin Seo <tkflqnf816@gmail.com>
2023-05-02 14:18:25 +0900

    풀 전 임시 커밋

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -4,8 +4,94 @@ import imgLogo from "../Assets/logo.png";
 import { Link } from "react-router-dom";
 import Footer from "../Components/Footer";
 import NavigationBar from "../Components/NavigationBar";
+import { useState } from "react";
+import axios from "axios";
 
 function Login() {
+  const [inputId, setInputId] = useState("");
+  const [inputPw, setInputPw] = useState("");
+  const [Role, setRole] = useState("회원");
+
+  const handleInputId = (e) => {
+    setInputId(e.target.value);
+  };
+
+  const handleInputPw = (e) => {
+    setInputPw(e.target.value);
+  };
+
+  const handleRole = (e) => {
+    setRole(e.target.value);
+  };
+
+  const onClickLogin = () => {
+    console.log("click login");
+    console.log("ID : ", inputId);
+    console.log("PW : ", inputPw);
+
+    if (Role === "회원") {
+      axios
+        .post("http://localhost:8080/user/login", {
+          email: inputId,
+          password: inputPw,
+        })
+        .then((res) => {
+          console.log(res);
+          console.log("res.data.email :: ", res.data.email);
+          console.log("res.data.msg :: ", res.data.msg);
+          if (res.data.email === undefined) {
+            // id 일치하지 않는 경우 user_Id = undefined, msg = '입력하신 id 가 일치하지 않습니다.'
+            console.log("======================", res.data.msg);
+            alert("입력하신 id 가 일치하지 않습니다.");
+            // document.location.href = "/login";
+          } else if (res.data.email === null) {
+            // id는 있지만, pw 는 다른 경우 user_Id = null , msg = undefined
+            console.log(
+              "======================",
+              "입력하신 비밀번호 가 일치하지 않습니다."
+            );
+            alert("입력하신 비밀번호 가 일치하지 않습니다.");
+          } else if (res.data.email === inputId) {
+            // id, pw 모두 일치 userId = userId1, msg = undefined
+            console.log("======================", "로그인 성공");
+            sessionStorage.setItem("user_name", res.data.name); // sessionStorage에 name을 user_name이라는 key 값으로 저장
+          }
+          // 작업 완료 되면 페이지 이동(새로고침)
+        })
+        .catch();
+    } else if (Role === "플래너") {
+      axios
+        .post("http://localhost:8080/planner/login", {
+          email: inputId,
+          password: inputPw,
+        })
+        .then((res) => {
+          console.log(res);
+          console.log("res.data.user_Id :: ", res.data.user_Id);
+          console.log("res.data.msg :: ", res.data.msg);
+          if (res.data.email === undefined) {
+            // id 일치하지 않는 경우 user_Id = undefined, msg = '입력하신 id 가 일치하지 않습니다.'
+            console.log("======================", res.data.msg);
+            alert("입력하신 id 가 일치하지 않습니다.");
+          } else if (res.data.email === null) {
+            // id는 있지만, pw 는 다른 경우 user_Id = null , msg = undefined
+            console.log(
+              "======================",
+              "입력하신 비밀번호 가 일치하지 않습니다."
+            );
+            alert("입력하신 비밀번호 가 일치하지 않습니다.");
+          } else if (res.data.email === inputId) {
+            // id, pw 모두 일치 userId = userId1, msg = undefined
+            console.log("======================", "로그인 성공");
+            sessionStorage.setItem("planner_name", res.data.name); // sessionStorage에 name을 planner_name이라는 key 값으로 저장
+          }
+          // 작업 완료 되면 페이지 이동(새로고침)
+          document.location.href = "/";
+        })
+        .catch();
+    }
+  };
+
   return (
     <div className="mainlayout">
       <NavigationBar title={"로그인"} />
@@ -27,16 +113,66 @@ function Login() {
               <div className="mb-3">
                 <input
                   type="text"
-                  className="inputarea"
+                  className="form-control inputarea"
                   placeholder="아이디(이메일)"
                   required=""
+                  value={inputId}
+                  onChange={handleInputId}
                 />
                 <input
                   type="password"
-                  className="inputarea"
+                  className="form-control inputarea"
                   placeholder="비밀번호"
                   required=""
+                  value={inputPw}
+                  onChange={handleInputPw}
                 />
+                <div class="input-group" id="Role" style={{ width: 256 }}>
+                  <div class="input-group-text">
+                    <input
+                      class="form-check-input mt-0"
+                      type="radio"
+                      value="회원"
+                      name="Role"
+                      htmlFor="회원"
+                      checked={Role === "회원"}
+                      onChange={handleRole}
+                      aria-label="Radio button for following text input"
+                      style={{ cursor: "pointer" }}
+                    />
+                  </div>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="custom"
+                    aria-label="custom btn"
+                    value="회원"
+                    disabled
+                    style={{ background: "white" }}
+                  />
+                  <div class="input-group-text">
+                    <input
+                      class="form-check-input mt-0"
+                      type="radio"
+                      value="플래너"
+                      name="Role"
+                      checked={Role === "플래너"}
+                      htmlFor="플래너"
+                      onChange={handleRole}
+                      aria-label="Radio button for following text input"
+                      style={{ cursor: "pointer" }}
+                    />
+                  </div>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="planner"
+                    aria-label="palnner btn"
+                    value="플래너"
+                    disabled
+                    style={{ background: "white" }}
+                  />
+                </div>
               </div>
               <Link to="/passwordSearch" className="searchmessage">
                 비밀번호를 잊으셨나요?
@@ -45,7 +181,7 @@ function Login() {
             <div className="col"></div>
           </div>
           <br />
-          <button type="submit" className="btn-custom1">
+          <button type="button" className="btn-custom1" onClick={onClickLogin}>
             로그인
           </button>
         </form>
--- src/Pages/PasswordSearch.js
+++ src/Pages/PasswordSearch.js
@@ -4,8 +4,70 @@ import "../Css/PasswordSearch.css";
 import { Link } from "react-router-dom";
 import Footer from "../Components/Footer";
 import NavigationBar from "../Components/NavigationBar";
+import { useState } from "react";
+import axios from "axios";
 
 function PasswordSearch() {
+  const [inputId, setInputId] = useState("");
+  const [Role, setRole] = useState("회원");
+
+  const handleInputId = (e) => {
+    setInputId(e.target.value);
+  };
+
+  const handleRole = (e) => {
+    setRole(e.target.value);
+  };
+
+  const onClicksend = () => {
+    console.log("click send");
+    console.log("ID : ", inputId);
+
+    if (Role === "회원") {
+      axios
+        .post("http://localhost:8080/user/login", {
+          email: inputId,
+        })
+        .then((res) => {
+          console.log(res);
+          console.log("res.data.user_Id :: ", res.data.user_Id);
+          if (res.data.email === undefined) {
+            // id 일치하지 않는 경우 user_Id = undefined, msg = '입력하신 id 가 일치하지 않습니다.'
+            console.log("======================", res.data.msg);
+            alert("입력하신 id 가 일치하지 않습니다.");
+          } else if (res.data.email === inputId) {
+            // id, pw 모두 일치 userId = userId1, msg = undefined
+            console.log("======================", "로그인 성공");
+            sessionStorage.setItem("user_name", res.data.name); // sessionStorage에 name을 user_name이라는 key 값으로 저장
+          }
+          // 작업 완료 되면 페이지 이동(새로고침)
+          // document.location.href = "/";
+        })
+        .catch();
+    } else if (Role === "플래너") {
+      axios
+        .post("http://localhost:8080/plannerlogin", {
+          email: inputId,
+        })
+        .then((res) => {
+          console.log(res);
+          console.log("res.data.user_Id :: ", res.data.user_Id);
+          if (res.data.email === undefined) {
+            // id 일치하지 않는 경우 user_Id = undefined, msg = '입력하신 id 가 일치하지 않습니다.'
+            console.log("======================", res.data.msg);
+            alert("입력하신 id 가 일치하지 않습니다.");
+          } else if (res.data.email === inputId) {
+            // id, pw 모두 일치 userId = userId1, msg = undefined
+            console.log("======================", "로그인 성공");
+            sessionStorage.setItem("planner_name", res.data.name); // sessionStorage에 name을 planner_name이라는 key 값으로 저장
+          }
+          // 작업 완료 되면 페이지 이동(새로고침)
+          // document.location.href = "/";
+        })
+        .catch();
+    }
+  };
+
   return (
     <div className="mainlayout">
       <NavigationBar title={"비밀번호 찾기"} />
@@ -36,13 +98,65 @@ function PasswordSearch() {
           </div>
         </div>
         <div className="row">
-          <div className="col-2"></div>
-          <div className="col-8">
+          <div className="col-3"></div>
+          <div className="col-6">
             <div className="infotext3">
-              <input type="text" className="inputarea" placeholder="이메일" />
+              <input
+                type="text"
+                className="inputarea"
+                placeholder="이메일"
+                value={inputId}
+                onChange={handleInputId}
+              />
+            </div>
+            <div class="input-group" id="Role" style={{ width: 256 }}>
+              <div class="input-group-text">
+                <input
+                  class="form-check-input mt-0"
+                  type="radio"
+                  value="회원"
+                  name="Role"
+                  htmlFor="회원"
+                  checked={Role === "회원"}
+                  onChange={handleRole}
+                  aria-label="Radio button for following text input"
+                  style={{ cursor: "pointer" }}
+                />
+              </div>
+              <input
+                type="text"
+                class="form-control"
+                id="custom"
+                aria-label="custom btn"
+                value="회원"
+                disabled
+                style={{ background: "white" }}
+              />
+              <div class="input-group-text">
+                <input
+                  class="form-check-input mt-0"
+                  type="radio"
+                  value="플래너"
+                  name="Role"
+                  checked={Role === "플래너"}
+                  htmlFor="플래너"
+                  onChange={handleRole}
+                  aria-label="Radio button for following text input"
+                  style={{ cursor: "pointer" }}
+                />
+              </div>
+              <input
+                type="text"
+                class="form-control"
+                id="planner"
+                aria-label="palnner btn"
+                value="플래너"
+                disabled
+                style={{ background: "white" }}
+              />
             </div>
           </div>
-          <div className="col-2"></div>
+          <div className="col-3"></div>
         </div>
         <div className="row">
           <div className="col-2"></div>
@@ -53,6 +167,7 @@ function PasswordSearch() {
                 className="btn-custom1"
                 data-bs-toggle="modal"
                 data-bs-target="#exampleModal"
+                onClick={onClicksend}
               >
                 임시 비밀번호 전송
               </button>
