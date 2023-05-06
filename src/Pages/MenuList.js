2e6af04880c9d4ea9189fec1522fa7f4760adaf3
sungjun son <o_nj_u@naver.com>
2023-05-06 11:17:48 +0900

    fix : className 변경, 가독성 향상(?)

--- src/Pages/MenuList.js
+++ src/Pages/MenuList.js
@@ -1,53 +1,44 @@
 import React from "react";
-import "../Css/main.css";
-import Footer from "../Components/Footer";
-import NavigationBar from "../Components/NavigationBar";
-import "../Css/menuList.css";
 import { Link } from "react-router-dom";
+import NavigationBar from "../Components/NavigationBar";
+import Footer from "../Components/Footer";
+import "../Css/menuList.css";
+
+const menuItems = [
+  { name: "웨딩홀", link: "./weddinghall" },
+  { name: "스튜디오", link: "./studio" },
+  { name: "드레스", link: "./dress" },
+  { name: "메이크업", link: "./makeup" },
+  { name: "사진/영상", link: "./photo-video" },
+  { name: "신혼여행", link: "./honeymoon" },
+  { name: "남성예복", link: "./groom-wear" },
+  { name: "부케", link: "./bouquet" },
+];
+
+const MenuItem = ({ name, link }) => {
+  return (
+    <Link to={link} className="menu-list-item">
+      <div className="menu-list-item-text">{name}</div>
+    </Link>
+  );
+};
 
 const MenuList = () => {
   const title = "메뉴";
-  const menuLeft = [
-    { name: "웨딩홀", link: "./weddinghall" },
-    { name: "스튜디오", link: "./studio" },
-    { name: "드레스", link: "./dress" },
-    { name: "메이크업", link: "./makeup" },
-  ];
-
-  const menuRight = [
-    { name: "사진/영상", link: "./photo-video" },
-    { name: "신혼여행", link: "./honeymoon" },
-    { name: "남성예복", link: "./groom-wear" },
-    { name: "부케", link: "./bouquet" },
-  ];
 
   return (
     <div className="mainlayout">
       <NavigationBar title={title} />
-      <div className="menu_content">
-        <div className="menu_list">
-          <div className="menu_list_left">
-            {menuLeft.map((menu, index) => (
-              <Link
-                key={index}
-                to={menu.link}
-                className="menu_list_item"
-                style={{ color: "#000" }}
-              >
-                <span className="menu_list_item_text">{menu.name}</span>
-              </Link>
+      <div className="menu-content">
+        <div className="menu-list">
+          <div className="menu-list-left">
+            {menuItems.slice(0, 4).map((item, index) => (
+              <MenuItem key={index} {...item} />
             ))}
           </div>
-          <div className="menu_list_right">
-            {menuRight.map((menu, index) => (
-              <Link
-                key={index}
-                to={menu.link}
-                className="menu_list_item"
-                style={{ color: "#000" }}
-              >
-                <span className="menu_list_item_text">{menu.name}</span>
-              </Link>
+          <div className="menu-list-right">
+            {menuItems.slice(4).map((item, index) => (
+              <MenuItem key={index} {...item} />
             ))}
           </div>
         </div>
