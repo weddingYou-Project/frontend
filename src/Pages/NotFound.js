73d4f9ee50e0cdb4c8d35eb81060bc9de5a524a4
diana815 <diana815@naver.com>
2023-05-08 09:18:44 +0900

    feat : notfound page img random으로 보여주기

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,18 +1,49 @@
 import Footer from "../Components/Footer";
+import Loadingimg1 from "../Assets/loading img1.jpg";
+import Loadingimg2 from "../Assets/loading img2.jpg";
+import Loadingimg3 from "../Assets/loading img3.jpg";
 
 function NotFound() {
+  const num = Math.floor(Math.random() * 3);
+  console.log(num);
+  const images = [Loadingimg1, Loadingimg2, Loadingimg3];
   return (
-    <div className="mainlayout">
+    <div
+      className="mainlayout"
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <div
         style={{
           alignItems: "center",
           justifyContent: "center",
+          flexDirection: "column",
           display: "flex",
           margin: "0 auto",
           height: "100%",
         }}
       >
-        <p style={{ fontSize: "2em" }}>접근할 수 없는 페이지입니다!😢</p>
+        <p
+          style={{
+            fontSize: "1.5em",
+            zIndex: 1,
+            marginBottom: "30px",
+            marginTop: "-30px",
+          }}
+        >
+          Access Denied!😢
+        </p>
+        <img
+          src={images[num]}
+          alt=""
+          style={{ width: "100%", height: "400px", marginBottom: "30px" }}
+        />
+        <button type="button" className="btn-colour-1 ">
+          홈으로
+        </button>
       </div>
       <Footer />
     </div>
