215346f07857387fee07969d6a89b1d315833149
diana815 <diana815@naver.com>
2023-05-08 09:20:34 +0900

    feat : notfound page 에서홈으로 이동하는 버튼 링크 연결

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -2,11 +2,13 @@ import Footer from "../Components/Footer";
 import Loadingimg1 from "../Assets/loading img1.jpg";
 import Loadingimg2 from "../Assets/loading img2.jpg";
 import Loadingimg3 from "../Assets/loading img3.jpg";
+import { useNavigate } from "react-router-dom";
 
 function NotFound() {
   const num = Math.floor(Math.random() * 3);
-  console.log(num);
+
   const images = [Loadingimg1, Loadingimg2, Loadingimg3];
+  const navigate = useNavigate();
   return (
     <div
       className="mainlayout"
@@ -41,7 +43,13 @@ function NotFound() {
           alt=""
           style={{ width: "100%", height: "400px", marginBottom: "30px" }}
         />
-        <button type="button" className="btn-colour-1 ">
+        <button
+          type="button"
+          className="btn-colour-1 "
+          onClick={() => {
+            navigate("/");
+          }}
+        >
           홈으로
         </button>
       </div>
