c57a93576b744d6ba1e81c9783fa8eb7447a6030
diana815 <diana815@naver.com>
2023-05-08 16:18:57 +0900

    feat: home main page mainlayout 생성

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,18 @@
+import "../Css/footer.css";
+import "../Css/main.css";
+import Footer from "../Components/Footer.js";
+import NavigationBar from "../Components/NavigationBar";
+
 function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <div class="mainlayout">
+        <NavigationBar />
+        <div class="content"></div>
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
