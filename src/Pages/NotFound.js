ba7ecddeb870ddd8c49834a87111820fa2bf6f72
diana815 <diana815@naver.com>
2023-05-08 09:43:31 +0900

    feat : notfoundpage에 backbutton 추가

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -3,57 +3,85 @@ import Loadingimg1 from "../Assets/loading img1.jpg";
 import Loadingimg2 from "../Assets/loading img2.jpg";
 import Loadingimg3 from "../Assets/loading img3.jpg";
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import BackButton from "../Components/Backbutton";
+import "../Css/Signup.css";
 
 function NotFound() {
   const num = Math.floor(Math.random() * 3);
 
   const images = [Loadingimg1, Loadingimg2, Loadingimg3];
   const navigate = useNavigate();
+  let [top, setTop] = useState(20);
+  let [opacity, setOpacity] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTop(120);
+      setOpacity(1);
+    }, 200);
+  });
   return (
     <div
-      className="mainlayout"
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
+      style={{ minHeight: "100vh", height: "700px", width: "100%", zIndex: 1 }}
     >
       <div
+        className="mainlayout"
         style={{
-          alignItems: "center",
+          display: "flex",
           justifyContent: "center",
           flexDirection: "column",
-          display: "flex",
-          margin: "0 auto",
-          height: "100%",
         }}
       >
-        <p
+        <div
           style={{
-            fontSize: "1.5em",
-            zIndex: 1,
-            marginBottom: "30px",
-            marginTop: "-30px",
+            display: "flex",
+            justifyContent: "start",
+            marginLeft: "10px",
           }}
         >
-          Access Denied!😢
-        </p>
-        <img
-          src={images[num]}
-          alt=""
-          style={{ width: "100%", height: "400px", marginBottom: "30px" }}
-        />
-        <button
-          type="button"
-          className="btn-colour-1 "
-          onClick={() => {
-            navigate("/");
+          <BackButton />
+        </div>
+        <div
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+            display: "flex",
+            margin: "0 auto",
+            height: "100%",
           }}
         >
-          홈으로
-        </button>
+          <p
+            style={{
+              fontSize: "1.5em",
+              zIndex: 1,
+              marginBottom: "30px",
+              marginTop: "-20px",
+              top: top,
+              opacity: opacity,
+            }}
+            className="Signup-SuccessMessage"
+          >
+            Access Denied!😢
+          </p>
+          <img
+            src={images[num]}
+            alt=""
+            style={{ width: "100%", height: "400px", marginBottom: "30px" }}
+          />
+          <button
+            type="button"
+            className="btn-colour-1 "
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            홈으로
+          </button>
+        </div>
+        <Footer />
       </div>
-      <Footer />
     </div>
   );
 }
