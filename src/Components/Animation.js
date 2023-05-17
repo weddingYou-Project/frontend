f85babc284a28d2fef1748b70d4f1e136e982231
diana815 <diana815@naver.com>
2023-05-17 13:05:42 +0900

    feat : 로딩시 이미지 lottie 이용

---
+++ src/Components/Animation.js
@@ -0,0 +1,118 @@
+import animationData from "../Animation/wedding.json";
+import animationData2 from "../Animation/love-hearts.json";
+import animationData3 from "../Animation/ornament-animation.json";
+import { useEffect, useRef, useState } from "react";
+import lottie from "lottie-web";
+
+const Animation = (props) => {
+  let [pTop, setPTop] = useState(60);
+  let [pOpacity, setPOpacity] = useState(0);
+  useEffect(() => {
+    setTimeout(() => {
+      setPTop(150);
+      setPOpacity(1);
+    }, 500);
+  }, []);
+  const container = useRef();
+  const container1 = useRef();
+  const container2 = useRef();
+  const container3 = useRef();
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: container3.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: animationData3,
+    });
+    lottie.loadAnimation({
+      container: container2.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: animationData2,
+    });
+    lottie.loadAnimation({
+      container: container1.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: animationData,
+    });
+  }, []);
+  return (
+    <div style={{ marginTop: "90px" }}>
+      <div
+        id="container"
+        ref={container}
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <p
+          style={{
+            fontSize: "1.5em",
+            zIndex: 1,
+            marginTop: "20px",
+            top: pTop,
+            opacity: pOpacity,
+            transition: "all 1s",
+            fontFamily: "Nanum Myeongjo",
+          }}
+        >
+          WEDDING YOU
+        </p>
+        <div
+          id="container3"
+          ref={container3}
+          style={{
+            width: "100%",
+            height: "200px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            marginTop: "-180px",
+            marginBottom: "-20px",
+          }}
+        ></div>
+        <div
+          id="container2"
+          ref={container2}
+          style={{
+            width: "100%",
+            height: "200px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+
+            marginTop: "-40px",
+          }}
+        ></div>
+        <div
+          id="container1"
+          ref={container1}
+          style={{
+            width: "100%",
+            height: "400px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            paddingLeft: "60px",
+            paddingRight: "20px",
+            marginTop: "-20px",
+          }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Animation;
