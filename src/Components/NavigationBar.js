1f8e54dd2da9e964897938caf7a7bbebec407df3
diana815 <diana815@naver.com>
2023-05-18 11:35:03 +0900

    feat : navigationbar에 update버튼 만들기

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -1,32 +1,99 @@
 import BackButton from "./Backbutton";
 import "../Css/NavigationBar.css";
 import MyEstimate from "./MyEstimate";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
-function NavigationBar({ title }) {
+function NavigationBar({ title, goUpdate }) {
   const url = useLocation();
   const path = url.pathname;
+
+  const [navigationBorder, setNavigationBorder] = useState(false);
+  window.addEventListener("scroll", () => {
+    console.log(document.body.clientHeight);
+    console.log("top:" + window.scrollY);
+    if (window.scrollY === 0) {
+      setNavigationBorder(true);
+    } else {
+      setNavigationBorder(false);
+    }
+  });
+
+  const navigate = useNavigate();
   return (
-    <div className="navigationbar">
-      <BackButton />
-      <p style={{ fontSize: "1.8em" }}>{title} </p>
-      {/* {path.indexOf("likeList") === 1 ? (
+    <div>
+      {navigationBorder === true ? (
         <div
-          className="btn btn-primary"
+          className="navigationbar"
           style={{
-            height: "40px",
-            marginTop: "10px",
-            marginRight: "-40px",
-            marginLeft: "-30px",
+            position: "fixed",
+            backgroundColor: "white",
+            borderTop: "1px solid rgb(173, 166, 166)",
+            width: "556px",
+            borderRadius: "10px 10px 0  0",
+            boxSizing: "border-box",
+            top: 0,
           }}
         >
-          update
+          <BackButton />
+          <p style={{ fontSize: "1.8em" }}>{title} </p>
+          {path.indexOf("likeList") === 1 ? (
+            <button
+              className="btn btn-primary"
+              style={{
+                height: "40px",
+                marginTop: "10px",
+                marginRight: "-40px",
+                marginLeft: "-30px",
+              }}
+              onClick={(e) => {
+                goUpdate();
+              }}
+            >
+              update
+            </button>
+          ) : null}
+          {path.indexOf("mypage") === 1 && path.indexOf("userupdate") === -1 ? (
+            <MyEstimate />
+          ) : (
+            <div className="empty"></div>
+          )}
         </div>
-      ) : null} */}
-      {path.indexOf("mypage") === 1 && path.indexOf("userupdate") === -1 ? (
-        <MyEstimate />
       ) : (
-        <div className="empty"></div>
+        <div
+          className="navigationbar"
+          style={{
+            position: "fixed",
+            backgroundColor: "white",
+            width: "556px",
+            boxSizing: "border-box",
+            top: 0,
+          }}
+        >
+          <BackButton />
+          <p style={{ fontSize: "1.8em" }}>{title} </p>
+          {path.indexOf("likeList") === 1 ? (
+            <button
+              className="btn btn-primary"
+              style={{
+                height: "40px",
+                marginTop: "10px",
+                marginRight: "-40px",
+                marginLeft: "-30px",
+              }}
+              onClick={(e) => {
+                goUpdate();
+              }}
+            >
+              update
+            </button>
+          ) : null}
+          {path.indexOf("mypage") === 1 && path.indexOf("userupdate") === -1 ? (
+            <MyEstimate />
+          ) : (
+            <div className="empty"></div>
+          )}
+        </div>
       )}
     </div>
   );
