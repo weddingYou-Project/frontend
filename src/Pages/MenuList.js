732a1014aa00ba13b161d5ac3b5a3f34712badd1
diana815 <diana815@naver.com>
2023-05-19 11:38:21 +0900

    style: service-menu-list 스타일 추가, menulist에 이미지 추가

--- src/Pages/MenuList.js
+++ src/Pages/MenuList.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import NavigationBar from "../Components/NavigationBar";
 import Footer from "../Components/Footer";
 import "../Css/menuList.css";
+import Loadingimg1 from "../Assets/loading img1.jpg";
+import Loadingimg2 from "../Assets/loading img2.jpg";
+import Loadingimg3 from "../Assets/loading img3.jpg";
 
 const Category1 = [
   { label: "웨딩홀", value: "weddinghall" },
@@ -20,7 +24,26 @@ const Service = [
 
 const MenuList = () => {
   const title = "메뉴";
+  const [num, setNum] = useState(0);
 
+  const images = [Loadingimg1, Loadingimg2, Loadingimg3];
+  const [selectImg, setSelectImg] = useState();
+  let [imgTop, setImgTop] = useState(120);
+  let [imgOpacity, setImgOpacity] = useState(0);
+  let [pTop, setPTop] = useState(60);
+  let [pOpacity, setPOpacity] = useState(0);
+  useEffect(() => {
+    setTimeout(() => {
+      setImgTop(170);
+      setImgOpacity(1);
+    }, 500);
+    setTimeout(() => {
+      setPTop(150);
+      setPOpacity(1);
+    }, 500);
+    setNum(Math.floor(Math.random() * 3));
+    setSelectImg(images[num]);
+  }, []);
   return (
     <div className="mainlayout">
       <NavigationBar title={title} />
@@ -31,7 +54,7 @@ const MenuList = () => {
               <Link
                 className="menu-list-item"
                 to={`/menu/${category1.value}`}
-                style={{ fontSize: "0.9em" }}
+                style={{ fontSize: "0.8em" }}
               >
                 {category1.label}
               </Link>
@@ -39,20 +62,86 @@ const MenuList = () => {
           </div>
         ))}
       </div>
-      <div className="service-menu-list">
+      <hr style={{ width: "80%", marginLeft: "60px" }} />
+      <div
+        className="service-menu-list"
+        style={{ width: "100%", margin: "0 auto" }}
+      >
         {Service.map((service, index) => (
           <div key={index} className="menu-list-item-container">
             <h2 style={{ marginTop: "20px" }}>
               <Link
                 className="menu-list-item"
                 to={`/${service.value}`}
-                style={{ fontSize: "0.9em" }}
+                style={{ fontSize: "0.8em" }}
               >
                 {service.label}
               </Link>
             </h2>
           </div>
         ))}
+      </div>
+      <div style={{ display: "flex" }}>
+        <img
+          src={images[num]}
+          alt=""
+          style={{
+            display: "block",
+            width: "250px",
+            height: "300px",
+            marginBottom: "80px",
+            transition: "all 1s",
+            marginTop: "60px",
+            marginLeft: "50px",
+            top: imgTop,
+            opacity: imgOpacity,
+          }}
+        />
+        <div>
+          <p
+            style={{
+              fontSize: "1.4em",
+              zIndex: 1,
+              marginTop: "100px",
+              top: pTop,
+              opacity: pOpacity,
+              transition: "all 2s",
+              fontFamily: "Nanum Myeongjo",
+              color: "#fe5278",
+            }}
+          >
+            &nbsp;&nbsp;&nbsp;BE THE REAL YOU <br />
+          </p>
+          <p
+            style={{
+              fontSize: "1.3em",
+              zIndex: 1,
+              top: pTop,
+              opacity: pOpacity,
+              transition: "all 2s",
+              fontFamily: "Nanum Myeongjo",
+              color: "#ff007f",
+            }}
+          >
+            IT'S YOUR SPECIAL DAY! <br />
+          </p>
+          <p
+            style={{
+              fontSize: "1em",
+              zIndex: 1,
+              marginTop: "70px",
+              top: pTop,
+              opacity: pOpacity,
+              transition: "all 2s",
+              fontFamily: "Nanum Myeongjo",
+              marginRight: "10px",
+              color: "#061148",
+            }}
+          >
+            We bring you the best wedding service
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; from all over the world...
+          </p>
+        </div>
       </div>
       <Footer />
     </div>
