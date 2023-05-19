874c13b317329088a2f18131455da92ba623c436
diana815 <diana815@naver.com>
2023-05-19 14:43:29 +0900

    feat : 이미지 모달창 각 페이지 수정

--- src/Pages/Items/Weddinghall.js
+++ src/Pages/Items/Weddinghall.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
-import Modal from "react-modal";
 import axios from "axios";
 import NavigationBar from "../../Components/NavigationBar";
 import Footer from "../../Components/Footer";
@@ -12,34 +11,107 @@ import wh_2 from "./wh_2.jpg";
 const Weddinghall = ({ postSubmitted }) => {
   const { category1 } = useParams();
   const title = "웨딩홀";
+
   const category2 = ["일반", "호텔", "채플", "스몰", "야외", "전통혼례"];
   const [isAdmin, setIsAdmin] = useState(true);
   const [selectedCategory, setSelectedCategory] = useState(category2[0]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedImage, setSelectedImage] = useState(null);
   const [selectLikeState, setSelectLikeState] = useState(false);
   const [editMode, setEditMode] = useState(false);
   const [newTitle, setNewTitle] = useState("");
   const [newContent, setNewContent] = useState("");
-  const [itemId, setItemId] = useState(null);
+  // const [itemId, setItemId] = useState(null);
   const [file, setFile] = useState(null);
   const [images, setImages] = useState([
     { id: 1, url: wh_1, title: "1", content: "1" },
     { id: 2, url: wh_2, title: "2", content: "2" },
   ]);
 
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-    setEditMode(false);
+  const [previewImg, setPreviewImg] = useState([]);
+  const [itemId, setItemId] = useState([]);
+  const [item, setItem] = useState([]);
+  const [itemName, setItemName] = useState([]);
+  const [itemContent, setItemContent] = useState([]);
+  const [keyIndex, setKeyIndex] = useState([]);
+
+  let keyIndexArr = [];
+  let list = [];
+  let itemDataArr = [];
+  let previewImgArr = [];
+  let itemNameArr = [];
+  let itemContentArr = [];
+
+  const modalImg = useRef();
+  const modalImgContent = useRef();
+  const modalImgTitle = useRef();
+  const modalItemId = useRef();
+
+  useEffect(() => {
+    axios
+      .get(`/item/itemList/${title}/${selectedCategory}`)
+      .then((res) => {
+        const dataList = res.data;
+
+        if (dataList.length !== 0) {
+          let index = 0;
+          for (var i = 0; i < dataList.length; ) {
+            let dataUrl = "data:image/jpeg;base64," + dataList[i];
+            previewImgArr.push(dataUrl);
+            setPreviewImg(previewImgArr);
+            i++;
+
+            let newitemId = dataList[i];
+            list.push(newitemId);
+            setItemId(list);
+            keyIndexArr.push(index);
+            index++;
+            setKeyIndex(keyIndexArr);
+            i++;
+
+            axios
+              .get(`/item/getItemList/${newitemId}`)
+              .then((res) => {
+                let newItem = res.data;
+                itemDataArr.push(newItem);
+                itemDataArr.sort(function (a, b) {
+                  return new Date(b.itemWriteDate) - new Date(a.itemWriteDate);
+                });
+                setItem([...item, newItem]);
+                setItem(itemDataArr);
+              })
+              .catch((e) => {
+                console.log(e);
+              });
+
+            let newitemName = dataList[i];
+            itemNameArr.push(newitemName);
+            setItemName(itemNameArr);
+            i++;
+
+            let newitemContent = dataList[i];
+            itemContentArr.push(newitemContent);
+            setItemContent(itemContentArr);
+            i++;
+          }
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, [selectedCategory]);
+
+  console.log(itemName);
+  console.log(itemContent);
+
+  const showimgDetail = (e) => {
+    modalImg.current.src = e.target.dataset.bsSrc;
+    modalImg.current.dataset.category = e.target.dataset.bsCategory;
+    modalImgContent.current.innerText = e.target.dataset.bsItemcontent;
+    modalImgTitle.current.innerText = `- ${e.target.dataset.bsItemname} -`;
   };
 
   const handleCategoryClick = (category) => {
     setSelectedCategory(category);
-  };
-
-  const handleImageClick = (image) => {
-    setSelectedImage(image);
-    setIsModalOpen(true);
   };
 
   const manageLikeList = () => {
@@ -105,7 +177,6 @@ const Weddinghall = ({ postSubmitted }) => {
           (image) => image.id !== selectedImage.id
         );
         setImages(updatedImages);
-        setIsModalOpen(false);
       })
       .catch((err) => {
         console.error(err);
@@ -144,127 +215,23 @@ const Weddinghall = ({ postSubmitted }) => {
         ))}
       </div>
       <div className="image-wrapper">
-        {images.map((image) => (
+        {keyIndex.map((i) => (
           <img
-            key={image.id}
-            src={image.url}
-            alt={image.title}
-            onClick={() => handleImageClick(image)}
+            //   key={image.id}
+            src={previewImg[i]}
+            alt=""
+            onClick={showimgDetail}
             data-bs-toggle="modal"
             data-bs-target="#imgDetailModal"
             style={{ cursor: "pointer" }}
+            data-bs-src={previewImg[i]}
+            data-bs-category="웨딩홀"
+            data-bs-itemName={itemName[i]}
+            data-bs-itemContent={itemContent[i]}
           />
         ))}
       </div>
-      {/* {selectedImage && (
-        <Modal
-          isOpen={isModalOpen}
-          onRequestClose={handleCloseModal}
-          style={modalStyles}
-        >
-          {editMode ? (
-            <>
-              <img src={selectedImage.url} alt={selectedImage.title} />
-              <div className="post-inputwrap">
-                <input
-                  className="title-input"
-                  type="text"
-                  value={newTitle}
-                  onChange={(e) => setNewTitle(e.target.value)}
-                />
-                <textarea
-                  className="content-textarea"
-                  value={newContent}
-                  onChange={(e) => setNewContent(e.target.value)}
-                />
-                <input type="file" onChange={handleFileChange} />
-              </div>
-              <div className="button-wrap">
-                <button className="edit-button" onClick={() => editItem()}>
-                  저장
-                </button>
-                <button
-                  className="delete-button"
-                  type="button"
-                  onClick={() => setEditMode(false)}
-                >
-                  취소
-                </button>
-              </div>
-            </>
-          ) : (
-            <>
-              <img src={selectedImage.url} alt={selectedImage.title} />
-              <h2>{selectedImage.title}</h2>
-              <p>{selectedImage.content}</p>
-              {selectLikeState === true ? (
-                <button
-                  style={{
-                    width: "110px",
-                    marginBottom: "10px",
-                    fontSize: "1em",
-                    background: "none",
-                    border: "none",
-                    padding: "5px 20px",
-                  }}
-                  onClick={manageLikeList}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    fill="red"
-                    class="bi bi-heart-fill"
-                    viewBox="0 0 16 16"
-                    style={{ cursor: "pointer" }}
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
-                    />
-                  </svg>
-                  찜하기
-                </button>
-              ) : (
-                <button
-                  style={{
-                    width: "110px",
-                    marginBottom: "10px",
-                    fontSize: "1em",
-                    background: "none",
-                    border: "none",
-                    padding: "5px 20px",
-                  }}
-                  onClick={manageLikeList}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    fill="currentColor"
-                    class="bi bi-heart"
-                    viewBox="0 0 16 16"
-                    style={{ cursor: "pointer" }}
-                  >
-                    <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-                  </svg>
-                  찜하기
-                </button>
-              )}
-              {isAdmin && (
-                <div className="button-wrapper">
-                  <button className="edit-button" onClick={handleEditClick}>
-                    수정
-                  </button>
-                  <button className="delete-button" onClick={handleDeleteClick}>
-                    삭제
-                  </button>
-                </div>
-              )}
-            </>
-          )}
-        </Modal>
-      )} */}
+
       {isAdmin && (
         <button
           className="submit-button"
@@ -295,7 +262,7 @@ const Weddinghall = ({ postSubmitted }) => {
                 class="modal-title justify-content-center "
                 id="imgDetailModal"
                 style={{ fontSize: "1.9em" }}
-                //  ref={modalImgTitle}
+                ref={modalImgTitle}
               >
                 - -
               </h1>
@@ -335,7 +302,7 @@ const Weddinghall = ({ postSubmitted }) => {
                     marginLeft: "20px",
                   }}
                   alt=""
-                  //   ref={modalImg}
+                  ref={modalImg}
                 />
                 <div
                   style={{
@@ -352,7 +319,7 @@ const Weddinghall = ({ postSubmitted }) => {
                     border: "1px solid black",
                     padding: "10px",
                   }}
-                  //   ref={modalImgContent}
+                  ref={modalImgContent}
                 ></p>
               </div>
             </div>
