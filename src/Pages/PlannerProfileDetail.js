2bc8897fc5c86fed24eb01703e6eaf31e940a5e9
diana815 <diana815@naver.com>
2023-05-24 14:10:11 +0900

    feat : profileDetail 자기소개, 포트폴리오 부분 삽입

--- src/Pages/PlannerProfileDetail.js
+++ src/Pages/PlannerProfileDetail.js
@@ -15,6 +15,7 @@ function PlannerProfileDetail() {
   const [plannerYears, setPlannerYears] = useState(0);
   const [reviewCount, setReviewCount] = useState(0);
   const [matchingCount, setMatchingCount] = useState(0);
+  const [introduction, setIntroduction] = useState("");
 
   useEffect(() => {
     //플래너이름, 플래너 프로필 사진, 리뷰개수, 별점, 매칭수, 소개글 불러오기
@@ -25,7 +26,10 @@ function PlannerProfileDetail() {
       <NavigationBar title={`${plannerName}의 프로필`} />
       <br />
 
-      <div class="container text-center" style={{ marginTop: "100px" }}>
+      <div
+        class="container text-center"
+        style={{ marginTop: "100px", minHeight: "100vh" }}
+      >
         <div
           style={{
             marginRight: "8px",
@@ -68,6 +72,81 @@ function PlannerProfileDetail() {
             </div>
           </div>
         </div>
+        <div>
+          <div
+            style={{
+              display: "flex",
+              width: "100px",
+              fontSize: "1.8em",
+              justifyContent: "start",
+              marginLeft: "20px",
+              marginTop: "20px",
+            }}
+          >
+            자기소개
+          </div>
+          <textarea
+            style={{
+              marginTop: "20px",
+              borderRadius: "10px",
+              fontSize: "1.4em",
+            }}
+            name=""
+            id=""
+            cols="45"
+            rows="6"
+            placeholder="아직 자기소개가 없습니다!"
+            value={introduction}
+            disabled
+          ></textarea>
+        </div>
+        <div>
+          <div
+            style={{
+              display: "flex",
+              width: "120px",
+              fontSize: "1.8em",
+              justifyContent: "start",
+              marginLeft: "20px",
+              marginTop: "20px",
+              marginBottom: "10px",
+            }}
+          >
+            포트폴리오
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "end",
+              fontSize: "1.4em",
+              marginRight: "30px",
+            }}
+          >
+            총 매칭수 : {matchingCount}
+          </div>
+          <textarea
+            style={{
+              marginTop: "20px",
+              borderRadius: "10px",
+              fontSize: "1.4em",
+              border: "1px solid grey",
+              height: "180px",
+              width: "500px",
+              margin: "0 auto",
+            }}
+            name=""
+            id=""
+            cols="45"
+            rows="6"
+            placeholder=" 김00 - 별점 4.5/5.0 
+        이00 - 별점 3.0/5.0"
+            value={introduction}
+            disabled
+          ></textarea>
+        </div>
+        <button class="btn-colour-1" style={{ marginTop: "50px" }}>
+          견적요청
+        </button>
       </div>
     </div>
   );
