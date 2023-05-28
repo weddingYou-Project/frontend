3f93f3de101139f5ca3d51910ea90da3b2737fe1
diana815 <diana815@naver.com>
2023-05-28 15:19:19 +0900

    feat : plannerProfile 이미지와 플래너 이름 불러오기 기능 구현

--- src/Pages/PlannerProfile.js
+++ src/Pages/PlannerProfile.js
@@ -6,15 +6,18 @@ import NavigationBar from "../Components/NavigationBar";
 import React, { useState, useEffect, useLayoutEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import Animation from "../Components/Animation";
-import Loadingimg1 from "../Assets/loading img1.jpg";
 import axios from "axios";
+import defaultprofileimage from "../Assets/defaultprofileimage.jpg";
 
 function PlannerProfile() {
-  const [previewImg, setPreviewImg] = useState([]);
-  const [plannerName, setPlannerName] = useState("");
+  const [profileImg, setProfileImg] = useState([]);
+  const [plannerName, setPlannerName] = useState([]);
+  const [plannerEmail, setPlannerEmail] = useState([]);
   const [plannerYears, setPlannerYears] = useState(0);
   const [reviewCount, setReviewCount] = useState(0);
   const [matchingCount, setMatchingCount] = useState(0);
+  const [keyIndex, setKeyIndex] = useState([]);
+  // const [selectedPlannerEmail, setSelectedPlannerEmail] = useState("");
 
   const [selectedSort, setSelectedSort] = useState("정렬"); // 초기 버튼명 설정
   const navigate = useNavigate();
@@ -23,8 +26,11 @@ function PlannerProfile() {
     setSelectedSort(sort); // 선택한 정렬로 버튼명 변경
   };
 
-  const goProfileDetail = () => {
-    navigate(`/plannerprofiledetail`);
+  const goProfileDetail = (e) => {
+    const selectedPlannerEmail = e.target.dataset.bsPlanneremail;
+    navigate(`/plannerprofiledetail`, {
+      state: { plannerEmail: selectedPlannerEmail },
+    });
   };
 
   useEffect(() => {
@@ -40,6 +46,37 @@ function PlannerProfile() {
       .post(`/plannerProfile/getProfiles2`)
       .then((res) => {
         console.log(res);
+        const data = res.data;
+        if (data.length !== 0) {
+          let profileImgArr = [];
+          let plannerNameArr = [];
+          let plannerEmailArr = [];
+          let keyIndexArr = [];
+          for (let i = 0; i < data.length; i++) {
+            if (i % 3 === 0) {
+              if (data[i] === "null") {
+                profileImgArr.push(defaultprofileimage);
+              } else {
+                let img = "data:image/jpeg;base64," + data[i];
+                profileImgArr.push(img);
+              }
+
+              const index = i / 3;
+              keyIndexArr.push(index);
+              console.log(keyIndexArr);
+            } else if (i % 3 === 1) {
+              plannerNameArr.push(data[i]);
+            } else if (i % 3 === 2) {
+              plannerEmailArr.push(data[i]);
+            }
+          }
+          setKeyIndex(keyIndexArr);
+          setProfileImg(profileImgArr);
+          setPlannerName(plannerNameArr);
+          setPlannerEmail(plannerEmailArr);
+        } else {
+          setKeyIndex([]);
+        }
       })
       .catch((e) => {
         console.log(e);
@@ -101,16 +138,56 @@ function PlannerProfile() {
           </ul>
         </div>
       </div>
-      <div class="container text-center" style={{ marginTop: "10px" }}>
+
+      <div class="container text-center" style={{ marginTop: "-10px" }}>
         <div
           style={{
             marginRight: "8px",
           }}
         ></div>
-        <div class="row" style={{ marginTop: "20px" }}>
+        <div class="container text-center">
+          <div class="row row-cols-2">
+            {/* 이미지카드 */}
+            {keyIndex.length === 0 ? (
+              <div
+                class="text-start"
+                style={{
+                  marginLeft: "10px",
+                  fontSize: "1.5em",
+                  marginTop: "40px",
+                }}
+              >
+                결과가 없습니다.
+              </div>
+            ) : (
+              keyIndex.map((i) => (
+                <div class="col">
+                  <div class="card margT">
+                    <img
+                      style={{ height: "230px", cursor: "pointer" }}
+                      src={profileImg[i]}
+                      class="card-img-top"
+                      alt="..."
+                      data-bs-plannerEmail={plannerEmail[i]}
+                      onClick={goProfileDetail}
+                    />
+                    <div class="card-body">
+                      <p class="card-text">{plannerName[i]} &nbsp;&nbsp;</p>
+                    </div>
+                  </div>
+                </div>
+              ))
+            )}
+
+            {/* 이미지카드 */}
+          </div>
+          <br />
+          <div style={{ marginBottom: "100px" }}></div>
+        </div>
+        {/* <div class="row" style={{ marginTop: "20px" }}>
           <div class="col">
             <img
-              src={Loadingimg1}
+              // src={Loadingimg1}
               alt=""
               style={{
                 width: "200px",
@@ -126,7 +203,7 @@ function PlannerProfile() {
           </div>
           <div class="col" style={{ fontSize: "1.8em", marginLeft: "30px" }}>
             <img
-              src={Loadingimg1}
+              // src={Loadingimg1}
               alt=""
               style={{
                 width: "200px",
@@ -137,7 +214,7 @@ function PlannerProfile() {
             />
             <div>planner2</div>
           </div>
-        </div>
+        </div> */}
       </div>
       <Footer />
     </div>
