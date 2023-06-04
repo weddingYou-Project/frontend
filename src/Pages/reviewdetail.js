35ffbed6c09a24e6ac82cae7a0961f08029bdc13
diana815 <diana815@naver.com>
2023-06-04 23:27:11 +0900

    feat : reviewDetail에서 수정하기 기능 구현

--- src/Pages/reviewdetail.js
+++ src/Pages/reviewdetail.js
@@ -5,15 +5,15 @@ import NavigationBar from "../Components/NavigationBar";
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
-import moment from "moment";
-import "moment/locale/ko";
+import { FaStar } from "react-icons/fa";
+import styled from "styled-components";
 
 function Reviewdetail() {
   const [editMode, setEditMode] = useState(false);
   const [editedComment, setEditedComment] = useState("댓글 Smaple");
-
+  const ARRAY = [0, 1, 2, 3, 4];
   const [actionmode, setActionmode] = useState(0);
-
+  const [clicked, setClicked] = useState([false, false, false, false, false]);
   const { estimateId } = useLocation().state;
   console.log("estimateId=-=-=-=-=-=- : " + estimateId);
 
@@ -24,7 +24,68 @@ function Reviewdetail() {
   const [reviewDate, setReviewDate] = useState([]);
   const [reviewText, setReviewText] = useState([]);
   const [images, setImages] = useState([]);
+
+  const [previewUrl, setPreviewUrl] = useState([]);
+  const [imgName, setImgName] = useState([]);
+  const [fileType, setFileType] = useState([]);
+  const [originImagesFile, setOriginImagesFile] = useState([]);
+  const [imgArr, setImgArr] = useState([]);
+  const [plannerEmail, setPlannerEmail] = useState("");
   const navigate = useNavigate();
+
+  const handleStarClick = (index) => {
+    let clickStates = [...clicked];
+    for (let i = 0; i < 5; i++) {
+      clickStates[i] = i <= index ? true : false;
+    }
+    setClicked(clickStates);
+    const Rating = index + 1;
+    console.log(Rating);
+    setReviewStars(Rating);
+  };
+
+  const deleteImg = (index) => {
+    let previewUrlCopy = [...previewUrl];
+    let imgArrCopy = [...imgArr];
+    imgArrCopy.splice(index, 1);
+    previewUrlCopy.splice(index, 1);
+    setPreviewUrl(previewUrlCopy);
+    setImgArr(imgArrCopy);
+    // for (let i = 0; i < imgArrCopy.length; i++) {
+    //   console.log(i);
+    //   if(index===i){
+
+    //   }
+    //   if (copy[i].name === image) {
+    //     copy.splice(i, 1);
+    //     setImages(copy);
+    //     break;
+    //   }
+    // }
+  };
+
+  const handleImageChange = (event) => {
+    const selectedImage = event.target.files[0];
+    console.log(event.target.files);
+    const imgArr1 = [...imgArr];
+    imgArr1.push(selectedImage);
+    setImgArr(imgArr1);
+    console.log("+_+_+_+_+_+_++_+_+_+_+");
+    console.log(imgArr1);
+    // setImage(selectedImage);
+    try {
+      const fileReader = new FileReader();
+      const previewUrlArr = [...previewUrl];
+      fileReader.onload = () => {
+        previewUrlArr.push(fileReader.result);
+        setPreviewUrl(previewUrlArr);
+      };
+
+      fileReader.readAsDataURL(selectedImage);
+    } catch (e) {
+      setPreviewUrl(selectedImage);
+    }
+  };
   useEffect(() => {
     axios
       .get(`/estimateIdReview/${estimateId}`)
@@ -38,6 +99,7 @@ function Reviewdetail() {
         setReviewComments(data.comments);
         setReviewViews(data.reviewCounts);
         setReviewDate(data.reviewDate);
+        setPlannerEmail(data.plannerEmail);
         const imagearray = JSON.parse(data.reviewImg);
         const getImages = async () => {
           try {
@@ -69,6 +131,8 @@ function Reviewdetail() {
       });
   }, []);
 
+  useEffect(() => {}, [editMode]);
+
   const handleEditClick = () => {
     setEditMode(true);
   };
@@ -82,10 +146,145 @@ function Reviewdetail() {
   };
 
   const reviewUpdateForm = () => {
+    axios
+      .get(`/estimateIdReview/${estimateId}`)
+      .then((res) => {
+        console.log(res);
+        const data = res.data;
+        if (data != null) {
+          const reviewStars = data.reviewStars;
+          const text = data.reviewText;
+          console.log(reviewText);
+          let clickStates = [...clicked];
+          for (let i = 0; i < 5; i++) {
+            clickStates[i] = i < reviewStars ? true : false;
+          }
+          setClicked(clickStates);
+          const Rating = reviewStars;
+          console.log(Rating);
+          setReviewStars(Rating);
+          setReviewText(text);
+          const getImages = async () => {
+            try {
+              console.log(data.reviewImg);
+              const imagearray = JSON.parse(data.reviewImg);
+              console.log(imagearray);
+              const imgNameArr = [];
+              const fileTypeArr = [];
+              for (let i = 0; i < imagearray.length; i++) {
+                const img = imagearray[i];
+                const imgname = img.slice(
+                  img.indexOf("_") + 1,
+                  img.indexOf(".")
+                );
+                const fileType = img.slice(img.indexOf(".") + 1);
+                imgNameArr.push(imgname);
+                fileTypeArr.push(fileType);
+              }
+              setImgName(imgNameArr);
+              setFileType(fileTypeArr);
+              const imagePromises = imagearray.map((image) => {
+                return axios.get("/review/imageview", {
+                  params: { image },
+                  responseType: "blob",
+                });
+              });
+              const responses = await Promise.all(imagePromises);
+              const imgFileArr = [];
+              const imageUrls = responses.map((res, index) => {
+                const resdata = URL.createObjectURL(res.data);
+                console.log("aaaaaaaaa");
+                console.log(res.data);
+                const blob = res.data;
+                const file = new File([blob], imgNameArr[index], {
+                  type: `image/${fileTypeArr[index]}`,
+                });
+                imgFileArr.push(file);
+
+                const previewUrlArr = [];
+                const getImgSrc = async (event, files) => {
+                  const getPreviewUrls = imgFileArr.map((file) => {
+                    const selectedImage = file;
+                    console.log("fileReader.result");
+                    return new Promise((resolve, reject) => {
+                      const fileReader = new FileReader();
+                      fileReader.onload = async () => {
+                        try {
+                          const response = await fileReader.result;
+
+                          resolve(response);
+                        } catch (e) {
+                          reject(e);
+                        }
+                      };
+                      fileReader.onerror = (error) => {
+                        reject(error);
+                      };
+                      fileReader.readAsDataURL(selectedImage);
+                    });
+                  });
+                  const fileInfos = await Promise.all(getPreviewUrls);
+                  setPreviewUrl(fileInfos);
+                  return fileInfos;
+                };
+                getImgSrc();
+
+                return resdata;
+              });
+              console.log(imageUrls);
+              setImages(imageUrls);
+              setOriginImagesFile(imgFileArr);
+              setImgArr(imgFileArr);
+            } catch (e) {
+              console.log(e);
+            }
+          };
+          getImages();
+        } else {
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+
     setActionmode(1);
   };
 
-  const updateReview = () => {
+  const updateReview = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("reviewText", reviewText);
+    let ratingstars = rating;
+
+    if (isNaN(rating)) {
+      ratingstars = 0;
+    }
+    console.log(ratingstars);
+    formData.append("reviewStars", ratingstars);
+    console.log("+_+_+_+_+_+_+_+_+");
+    if (imgArr.length > 0) {
+      for (let i = 0; i < imgArr.length; i++) {
+        formData.append("reviewImg", imgArr[i]);
+      }
+    }
+
+    formData.append("userEmail", sessionStorage.getItem("email"));
+    formData.append("plannerEmail", plannerEmail);
+    formData.append("estimateId", estimateId);
+    if (reviewText === undefined || reviewText === "") {
+      alert("리뷰를 작성하시려면 리뷰 후기를 적어주세요!");
+    } else {
+      axios
+        .post("/reviews", formData)
+        .then((res) => {
+          console.log("성공:", res);
+          alert("리뷰 수정 완료!");
+          navigate(`/review`);
+        })
+        .catch((e) => {
+          console.log("실패:", e);
+        });
+    }
     setActionmode(0);
   };
 
@@ -351,39 +550,166 @@ function Reviewdetail() {
       <div className="mainlayout">
         <NavigationBar title={"글수정"} />
         <div style={{ height: 74 }}></div>
-        <div className="titleArea">
-          <p className="titleTxt">이용후기 TitleSample</p>
+        <div className="titleArea" style={{ display: "flex" }}>
+          <input
+            className="titleTxt"
+            type="text"
+            style={{
+              marginLeft: "20px",
+              marginRight: "20px",
+              height: "45px",
+              marginTop: "20px",
+              textAlign: "start",
+              paddingBottom: "20px",
+              borderRadius: "10px",
+            }}
+            value={reviewTitle}
+            onChange={(e) => {
+              setReviewTitle(e.target.value);
+            }}
+          ></input>
+          <div className="stars" style={{ marginTop: "10px" }}>
+            <Stars>
+              {ARRAY.map((el, idx) => {
+                return (
+                  <FaStar
+                    key={idx}
+                    size="25"
+                    onClick={() => handleStarClick(el)}
+                    className={clicked[el] && "yellowStar"}
+                  />
+                );
+              })}
+            </Stars>
+            <input type="hidden" value={rating} />
+          </div>
         </div>
         <hr />
         <div className="writeContent">
           <textarea
             className="form-control contentinput"
             rows="15"
-            placeholder="이용후기는 제목과 별점을 제외한 내용만 수정 가능합니다.
-            수정내용을 입력해주세요"
+            placeholder="수정내용을 입력해주세요"
+            value={reviewText}
+            onChange={(e) => {
+              setReviewText(e.target.value);
+            }}
             style={{ fontSize: 20 }}
           ></textarea>
         </div>
         <hr />
-        <div className="fileatt">
-          <p className="uploadphoto">사진 첨부</p>
+
+        <div className="photouploadsection">
+          <p className="uploadphoto" style={{ fontSize: "1.5em" }}>
+            사진 첨부
+          </p>
           <input
             type="file"
             multiple
             id="uploadimage"
             className="displaynone"
+            accept="image/*"
+            onChange={handleImageChange}
           />
-          <label htmlFor="uploadimage" className="cursor imageBtn">
+          <label
+            htmlFor="uploadimage"
+            style={{ fontSize: "1.5em" }}
+            className="cursor imageBtn"
+          >
             사진선택
           </label>
+
+          <div>
+            <h5>
+              고객 첨부이미지{" "}
+              {previewUrl.length !== 0 && <span>(클릭시 삭제됩니다)</span>}
+            </h5>
+            {previewUrl.length === 0 && <span>첨부 이미지가 없습니다.</span>}
+            <br></br>
+            {console.log("previewUrl")}
+            {console.log(previewUrl)}
+            {previewUrl.length !== 0 ? (
+              previewUrl.map((url, index) => {
+                return (
+                  <div key={index} style={{ marginBottom: "20px" }}>
+                    <button
+                      type="button"
+                      class="btn imgOverlay"
+                      // data-bs-toggle="modal"
+                      // data-bs-target={`#number${index.toString()}`}
+                      style={{
+                        width: "200px",
+                        pointer: "cursor",
+                        padding: 0,
+                        height: "100%",
+                        borderRadius: "10px",
+                        margin: 0,
+                      }}
+                      onClick={() => {
+                        deleteImg(index);
+                      }}
+                    >
+                      <img
+                        src={url}
+                        alt=""
+                        style={{
+                          display: "inline-block",
+                          width: "200px",
+                          height: "200px",
+
+                          borderRadius: "10px",
+                        }}
+                      />
+                    </button>
+                    {/* <ImagesView
+                      images={url}
+                      index={`number${index.toString()}`}
+                    /> */}
+                  </div>
+                );
+              })
+            ) : (
+              <div></div>
+              // <div style={{ marginTop: 5 }}>
+              //   {images.map((image, index) => {
+              //     return (
+              //       <div className="imagefilenamebox">
+              //         <div className="imagefilenamecontent">
+              //           <span>{image.name}</span>
+              //           <img
+              //             src={image}
+              //             width="40%"
+              //             height="40%"
+              //             style={{
+              //               float: "left",
+              //               width: "100%",
+              //               borderRadius: "10px",
+              //             }}
+              //             alt=""
+              //           />
+              //           <div
+              //             className="imagefilename-overlay cursor"
+              //             onClick={() => {
+              //               deleteimage(image.name);
+              //             }}
+              //           >
+              //             <i class="bi bi-x-lg"></i>
+              //           </div>
+              //         </div>
+              //       </div>
+              //     );
+              //   })}
+              // </div>
+            )}
+          </div>
         </div>
         <br />
-        <div className="writeBtnArea">
+        <div className="writeBtnArea" style={{ marginTop: "-40px" }}>
           <button className="writeBtn" onClick={updateReview}>
             수정하기
           </button>
         </div>
-        <div style={{ height: 90 }}></div>
+        <div style={{ height: 200 }}></div>
         <Footer />
       </div>
     );
@@ -408,3 +734,24 @@ const ImagesView = ({ images, index }) => {
     </div>
   );
 };
+const Stars = styled.div`
+  display: flex;
+  padding-top: 5px;
+
+  & svg {
+    color: gray;
+    cursor: pointer;
+  }
+
+  :hover svg {
+    color: #fcc419;
+  }
+
+  & svg:hover ~ svg {
+    color: gray;
+  }
+
+  .yellowStar {
+    color: #fcc419;
+  }
+`;
