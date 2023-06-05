e7b83fb6a8341b93ee9d79e75a456aea1a4291d7
KyeongSe Song <thdrudtp15@naver.com>
2023-06-05 14:28:47 +0900

    fix : 수정1

--- src/Pages/EstimateList.js
+++ src/Pages/EstimateList.js
@@ -247,54 +247,57 @@ const EstimateList = () => {
   return (
     <div className="mainlayout">
       <NavigationBar title={"견적서 목록"} />
-      <div
-        style={{
-          width: "560px",
-          position: "fixed",
-          bottom: "120px",
-          height: "50px",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "end",
-          alignItems: "end",
-          paddingRight: "23px",
-          paddingLeft: "50px",
-          paddingBottom: "10px",
-          zIndex: "999",
-        }}
-      >
-        <div style={{}}>
-          <div className="estimate-write-btn">
-            <i
-              class="bi bi-pencil-square"
-              style={{ marginLeft: "50px", zIndex: "999" }}
-            ></i>
-            <div
-              className="estimate-write-btn-overlay"
-              onClick={() => {
-                navigate("/estimateform");
-              }}
-              style={{
-                marginRight: "-20px",
-                marginLeft: "12px",
-                zIndex: "999",
-                height: "50px",
-              }}
-            >
-              <span>견적작성하기</span>
+      {sessionStorage.getItem("category") === "user" ? (
+        <div
+          style={{
+            width: "560px",
+            position: "fixed",
+            bottom: "120px",
+            height: "50px",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "end",
+            alignItems: "end",
+            paddingRight: "23px",
+            paddingLeft: "50px",
+            paddingBottom: "10px",
+            zIndex: "999",
+          }}
+        >
+          <div style={{}}>
+            <div className="estimate-write-btn">
+              <i
+                class="bi bi-pencil-square"
+                style={{ marginLeft: "50px", zIndex: "999" }}
+              ></i>
+              <div
+                className="estimate-write-btn-overlay"
+                onClick={() => {
+                  navigate("/estimateform");
+                }}
+                style={{
+                  marginRight: "-20px",
+                  marginLeft: "12px",
+                  zIndex: "999",
+                  height: "50px",
+                }}
+              >
+                <span>견적작성하기</span>
+              </div>
             </div>
           </div>
+          <div
+            className="scrolltop"
+            onClick={() => {
+              onScrollTop();
+            }}
+            style={{ marginRight: "5px" }}
+          >
+            <i class="bi bi-chevron-up"></i>
+          </div>
         </div>
-        <div
-          className="scrolltop"
-          onClick={() => {
-            onScrollTop();
-          }}
-          style={{ marginRight: "5px" }}
-        >
-          <i class="bi bi-chevron-up"></i>
-        </div>
-      </div>
+      ) : null}
+
       {/* <div
         className="scrolltop"
         onClick={() => {
